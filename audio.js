7d749784d85a1192979fe1cb41fb66fe85c77af9
Gregory Pappas <gnpappas0@gmail.com>
2023-04-29 21:40:56 -1000

    Icons to toggle playback; custom volumes

--- audio.js
+++ audio.js
@@ -1,15 +1,15 @@
 //? function controlling playback
-let playPause = (audio, button) => {
+let playPause = (audio, button, silence, sound) => {
     let isPlaying = false;
     button.addEventListener("click", () => {
         if (isPlaying) {
             audio.pause();
             isPlaying = false;
-            button.textContent = "Play";
+            button.textContent = silence;
         } else {
             audio.play();
             isPlaying = true;
-            button.textContent = "Pause";
+            button.textContent = sound;
         }
     });
 };
@@ -22,22 +22,38 @@ let volumeControl = (audio, volume) => {
 
 //? audio 1 start
 const one = new Howl({
-    src: ["./audio/one.mp3"],
+    src: ["./audio/one.ogg"],
     loop: true,
 });
 const volumeOne = document.getElementById("volumeOne");
 const buttonOne = document.getElementById("buttonOne");
-playPause(one, buttonOne);
+playPause(one, buttonOne, "🌩️", "⛈️");
 volumeControl(one, volumeOne);
 //?audio 1 end
 
 //? audio 2 start
 const two = new Howl({
-    src: ["./audio/two.mp3"],
+    src: ["./audio/two.ogg"],
     loop: true,
 });
 const volumeTwo = document.getElementById("volumeTwo");
 const buttonTwo = document.getElementById("buttonTwo");
-playPause(two, buttonTwo);
+playPause(two, buttonTwo, "🌬️", "🍃");
 volumeControl(two, volumeTwo);
 //?audio 2 end
+
+//? audio 3 start
+const three= new Howl({
+    src: ["./audio/three.ogg"],
+    loop: true,
+});
+const volumeThree = document.getElementById("volumeThree");
+const buttonThree = document.getElementById("buttonThree");
+playPause(three, buttonThree, "🧘", "🪷");
+volumeControl(three, volumeThree);
+//?audio 3 end
+window.onload = () => {
+    buttonOne.textContent = "🌩️";
+    buttonTwo.textContent = '🌬️';
+    buttonThree.textContent = '🧘';
+}
