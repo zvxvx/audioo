ed8c223aee106fa4c492adb7c3bdc9aafae8a068
Gregory Pappas <gnpappas0@gmail.com>
2023-05-26 19:29:42 -1000

    Cozy playlist added

--- README.md
+++ README.md
@@ -1,21 +1,10 @@
-# audi∞
+# audioo
 #### TO DO
-- [ ] support light and dark mode
-- [x] add an about page
-- [x] add a faq page
-- [x] tabs for different sound groups
-- [x] each sound group is to have its own theme
-- [x] add 'if no sound' in faq
-- [x] have about and faq on the same page as whatever audio is playing.
-- [x] make mobile view more malleable
-- [x] refine logo
 - [ ] kill/resume button?
 
 
 #### SOUNDS TO CONSIDER
 - [ ] feet crunching on snow
-- [x] ocean waves
-- [ ] electric sounds
 - [ ] rapids
 
 #### urban sounds
--- playlists/cozy/cozy.index.js
+++ playlists/cozy/cozy.index.js
@@ -2,63 +2,63 @@ import { playPause, volumeControl, audioGen} from '/audio.global.js';
 import {iconOne, iconTwo, iconThree, iconFour, iconFive, iconSix, buttonOne, buttonTwo, buttonThree, buttonFour, buttonFive, buttonSix, volumeOne, volumeTwo, volumeThree, volumeFour, volumeFive, volumeSix} from '../playlist.global.js'
 
 //? icon storage
-iconOne.src = "/img/bolt.png";
+iconOne.src = "./img/fireplace.png";
 iconOne.alt = "lightning";
 iconOne.style.opacity = "0.5";
 buttonOne.appendChild(iconOne);
-iconTwo.src = "/img/rain.png";
+iconTwo.src = "./img/rain.png";
 iconTwo.alt = "rain";
 iconTwo.style.opacity = "0.5";
 buttonTwo.appendChild(iconTwo);
-iconThree.src = "/img/windy.png";
+iconThree.src = "./img/subway.png";
 iconThree.alt = "wind";
 iconThree.style.opacity = "0.5";
 buttonThree.appendChild(iconThree);
-iconFour.src = "./img/chimes.png";
+iconFour.src = "./img/kitty.png";
 iconFour.alt = "chimes";
 iconFour.style.opacity = "0.5";
 buttonFour.appendChild(iconFour);
-iconFive.src = "./img/insect.png";
+iconFive.src = "./img/book.png";
 iconFive.alt = "insect";
 iconFive.style.opacity = "0.5";
 buttonFive.appendChild(iconFive);
-iconSix.src = "./img/water.png";
+iconSix.src = "./img/song.png";
 iconSix.alt = "water";
 iconSix.style.opacity = "0.5";
 buttonSix.appendChild(iconSix);
 
 //? audio 1 start
-let one = audioGen("/audio/one-thunder.mp3");
+let one = audioGen("./audio/one-fireplace.mp3");
 playPause(one, buttonOne, iconOne);
 volumeControl(one, volumeOne);
 //?audio 1 end
 
 //? audio 2 start
-let two = audioGen("/audio/two-rain.mp3");
+let two = audioGen("./audio/two-roofrain.mp3");
 playPause(two, buttonTwo, iconTwo);
 volumeControl(two, volumeTwo);
 //?audio 2 end
 
 //? audio 3 start
-let three = audioGen("/audio/three-wind.mp3");
+let three = audioGen("./audio/three-train.mp3");
 playPause(three, buttonThree, iconThree);
 volumeControl(three, volumeThree);
 //?audio 3 end
 
 //? audio 4 start
-let four = audioGen("./audio/four-chimes.mp3");
+let four = audioGen("./audio/four-purr.mp3");
 playPause(four, buttonFour, iconFour);
 volumeControl(four, volumeFour);
 //?audio 4 end
 
 //? audio 5 start
-let five = audioGen("./audio/five-insects.mp3");
+let five = audioGen("./audio/five-book.mp3");
 playPause(five, buttonFive, iconFive);
 volumeControl(five, volumeFive);
 //?audio 5 end
 
 //? audio 6 start
-let six = audioGen("./audio/six-water.mp3");
+let six = audioGen("./audio/six-lofi.mp3");
 playPause(six, buttonSix, iconSix);
 volumeControl(six, volumeSix);
 //?audio 6 end
