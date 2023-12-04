11921149f13d98a41ba9c65bf5bf955333cef79d
Greg Pappas <gnpappas@proton.me>
2023-12-03 20:13:24 -1000

    Add returning text demo

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ function App() {
   return (
     <>
       <div className="h-[100dvh] bg-purple-950/20">
-        <p className="text-white text-7xl text-slate-300 font-bold text-center">audioo</p>
+        <p className="text-7xl text-white text-slate-300 font-bold text-center">audioo</p>
+        <p className="text-white text-slate-300 font-bold text-center">is returning soon...</p>
       </div>
     </>
   )
