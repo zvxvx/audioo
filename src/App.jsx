76d850c75608f2db9c01020de5c10aef265c561f
Greg Pappas <gnpappas@proton.me>
2023-12-09 08:49:37 -1000

    Test front page before component conversion

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,20 @@
 import logo from "./assets/img/audiologo.svg";
+import Audio from "./Audio";
+import thunder from "./assets/audio/one-thunder.mp3";
+import lightning from "./assets/img/lightning.png";
+import rain from "./assets/audio/two-rain.mp3";
+import rainImg from "./assets/img/rain.png";
+import wind from "./assets/audio/three-wind.mp3";
+import windImg from "./assets/img/wind.png";
 function App() {
   return (
-    <div className="h-[100dvh] flex flex-col items-center bg-stone-800 text-slate-200">
-      <img src={logo} alt="audioo" className="w-1/2" />
-      <p className="">coming soon</p>
+    <div className="h-[100dvh] text-slate-200">
+      <img src={logo} alt="audioo" className="w-1/2 mx-auto" />
+      <div className="grid grid-cols-1 sm:grid-cols-3 justify-center">
+        <Audio url={thunder} img={lightning} />
+        <Audio url={rain} img={rainImg} />
+        <Audio url={wind} img={windImg} />
+      </div>
     </div>
   );
 }
