f97ea70b7397c13317b6edaf958c08e09f7776d6
Greg Pappas <gnpappas@proton.me>
2023-12-09 09:37:56 -1000

    Reinit

---
+++ vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import { imagetools } from "vite-imagetools";
+import react from "@vitejs/plugin-react-swc";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    imagetools({
+      defaultDirectives: (url) => {
+        if (url.searchParams.has("icon")) {
+          return new URLSearchParams("w=300&format=webp");
+        }
+        return new URLSearchParams();
+      },
+    }),
+  ],
+});
