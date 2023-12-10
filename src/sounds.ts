7fa404c0af3b6fc7f137c38ab993c8e6d6bb839c
Greg Pappas <gnpappas@proton.me>
2023-12-09 18:40:07 -1000

    Add backyard sounds

--- src/sounds.ts
+++ src/sounds.ts
@@ -4,6 +4,12 @@ import rain from "./assets/audio/two-rain.mp3";
 import rainImg from "./assets/img/rain.png?icon";
 import wind from "./assets/audio/three-wind.mp3";
 import windImg from "./assets/img/wind.png?icon";
+import insects from "./assets/playlists/backyard/audio/five-insects.mp3";
+import insImg from "./assets/playlists/backyard/img/insect.png?icon";
+import chimes from "./assets/playlists/backyard/audio/four-chimes.mp3";
+import chiImg from "./assets/playlists/backyard/img/chimes.png?icon";
+import water from "./assets/playlists/backyard/audio/six-water.mp3";
+import watImg from "./assets/playlists/backyard/img/water.png?icon";
 
 export const home = [
   {
@@ -20,5 +26,23 @@ export const home = [
     id: 2,
     sound: wind,
     image: windImg,
+  },
+];
+
+export const backyard = [
+  {
+    id: 0,
+    sound: chimes,
+    image: chiImg,
+  },
+  {
+    id: 1,
+    sound: insects,
+    image: insImg,
+  },
+  {
+    id: 2,
+    sound: water,
+    image: watImg,
   },
 ];
