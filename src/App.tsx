e767cc0165a8daf5cd3a159b21965cae1edce303
Greg Pappas <gnpappas@proton.me>
2023-12-09 19:08:50 -1000

    Move grid to components to increase flexibility

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
     <div className="h-[100dvh] bg-purple-950/30 text-slate-200">
       <div className="mx-auto w-5/6">
         <Header />
-        <div className="bg-slate-950/50 grid grid-cols-1 md:grid-cols-3 gap-5 justify-center p-5">
+        <div className="bg-slate-950/50 p-5">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/backyard" element={<Backyard />} />
