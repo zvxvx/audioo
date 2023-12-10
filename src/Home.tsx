c2e6be1986fe6c0ceb7969e3484f5444774c40b2
Greg Pappas <gnpappas@proton.me>
2023-12-09 16:13:07 -1000

    Generate sounds via map from ext obj

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,18 +1,16 @@
 import Audio from "./Audio";
-import thunder from "./assets/audio/one-thunder.mp3";
-import lightning from "./assets/img/lightning.png?icon";
-import rain from "./assets/audio/two-rain.mp3";
-import rainImg from "./assets/img/rain.png?icon";
-import wind from "./assets/audio/three-wind.mp3";
-import windImg from "./assets/img/wind.png?icon";
+import { home } from "./sounds";
+
 const Home = () => {
   return (
     <>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-5 justify-center">
-        <Audio url={thunder} img={lightning} />
-        <Audio url={rain} img={rainImg} />
-        <Audio url={wind} img={windImg} />
-      </div>
+      {home.map(({ id, sound, image }) => {
+        return (
+          <div key={id}>
+            <Audio url={sound} img={image} />
+          </div>
+        )
+      })}
     </>
   )
 }
