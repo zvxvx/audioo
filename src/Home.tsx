312272573337d1ba2ab34a9faba0196f909751e7
Greg Pappas <gnpappas@proton.me>
2023-12-09 19:10:08 -1000

    Update sound object and integrate grid

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,17 +1,20 @@
 import Audio from "./Audio";
 import News from "./News";
-import { home } from "./sounds";
+import home from "./assets/playlists/home";
 
 const Home = () => {
   return (
     <>
-      {home.map(({ id, sound, image }) => {
-        return (
-          <div key={id}>
-            <Audio url={sound} img={image} />
-          </div>
-        )
-      })}
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-5 justify-center">
+        {home.map(({ id, sound, image }) => {
+          return (
+            <div key={id}>
+              <Audio url={sound} img={image} />
+            </div>
+          )
+        })}
+      </div>
+      <News/>
     </>
   )
 }
