09efa4664457fbbce78fcd4c6417af3694e3e267
Greg Pappas <gnpappas@proton.me>
2023-12-09 19:26:18 -1000

    Map out cozy object

--- src/Cozy.tsx
+++ src/Cozy.tsx
@@ -1,6 +1,17 @@
+import Audio from "./Audio";
+import cozy from "./assets/playlists/cozy/cozy";
+
 const Cozy = () => {
   return (
-    <div>Cozy</div>
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-5 justify-center">
+      {cozy.map(({ id, sound, image }) => {
+        return (
+          <div key={id}>
+            <Audio url={sound} img={image} />
+          </div>
+        )
+      })}
+    </div>
   )
 }
 
