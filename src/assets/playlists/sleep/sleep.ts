ce3ea0cda17300231e60d7275639c198316ac861
Greg Pappas <gnpappas@proton.me>
2023-12-09 19:58:25 -1000

    Add sleep sound object

---
+++ src/assets/playlists/sleep/sleep.ts
@@ -0,0 +1,47 @@
+import drone from "./audio/one-drone.mp3";
+import droImg from "./img/one.png";
+import phase from "./audio/two-phase.mp3";
+import phaImg from "./img/two.png";
+import pulse from "./audio/three-pulse.mp3";
+import pulImg from "./img/three.png";
+import brown from "./audio/four-brown.mp3";
+import broImg from "./img/brownian.png";
+import pink from "./audio/five-pink.mp3";
+import pinkImg from "./img/pink.png";
+import fan from "./audio/six-fan.mp3";
+import fanImg from "./img/fan.png";
+
+const cozy = [
+  {
+    id: 0,
+    sound: drone,
+    image: droImg,
+  },
+  {
+    id: 1,
+    sound: phase,
+    image: phaImg,
+  },
+  {
+    id: 2,
+    sound: pulse,
+    image: pulImg,
+  },
+  {
+    id: 3,
+    sound: brown,
+    image: broImg,
+  },
+  {
+    id: 4,
+    sound: pink,
+    image: pinkImg,
+  },
+  {
+    id: 5,
+    sound: fan,
+    image: fanImg,
+  },
+];
+
+export default cozy;
