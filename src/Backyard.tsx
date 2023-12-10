cd1df2f9ae5c820f0b7c31a89c7d5e05f9d6e80f
Greg Pappas <gnpappas@proton.me>
2023-12-09 18:39:50 -1000

    Map out sounds

--- src/Backyard.tsx
+++ src/Backyard.tsx
@@ -1,7 +1,23 @@
+import Audio from "./Audio";
+import { home, backyard } from "./sounds";
 
 const Backyard = () => {
   return (
     <>
+      {home.map(({ id, sound, image }) => {
+        return (
+          <div key={id}>
+            <Audio url={sound} img={image} />
+          </div>
+        )
+      })}
+      {backyard.map(({ id, sound, image }) => {
+        return (
+          <div key={id}>
+            <Audio url={sound} img={image} />
+          </div>
+        )
+      })}
     </>
   )
 }
