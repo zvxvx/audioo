7f24dcc0fe94bf8827f85d3b6fe9dcae5c6719ab
Greg Pappas <gnpappas@proton.me>
2023-12-09 16:12:44 -1000

    Move news to show in home

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,12 @@ function App() {
     <div className="h-[100dvh] bg-purple-950/30 text-slate-200">
       <div className="mx-auto w-5/6">
         <Header />
-        <div className="bg-slate-950/50 p-5">
+        <div className="bg-slate-950/50 grid grid-cols-1 md:grid-cols-3 gap-5 justify-center">
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/news" element={<News />} />
           </Routes>
         </div>
+        <News/>
         <Footer />
       </div>
     </div>
