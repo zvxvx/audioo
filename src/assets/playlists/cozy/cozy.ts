e78c0e59f3a97fd5890493a181ec2549860eb706
Greg Pappas <gnpappas@proton.me>
2023-12-09 19:25:59 -1000

    Add cozy sound object

---
+++ src/assets/playlists/cozy/cozy.ts
@@ -0,0 +1,47 @@
+import fireplace from "./audio/one-fireplace.mp3";
+import fireImg from "./img/fireplace.png";
+import roof from "./audio/two-roofrain.mp3";
+import roofImg from "./img/rain.png";
+import train from "./audio/three-train.mp3";
+import trainImg from "./img/subway.png";
+import cat from "./audio/four-purr.mp3";
+import catImg from "./img/kitty.png";
+import book from "./audio/five-book.mp3";
+import bookImg from "./img/book.png";
+import lofi from "./audio/six-lofi.mp3";
+import lofiImg from "./img/song.png";
+
+const cozy = [
+  {
+    id: 0,
+    sound: fireplace,
+    image: fireImg,
+  },
+  {
+    id: 1,
+    sound: roof,
+    image: roofImg,
+  },
+  {
+    id: 2,
+    sound: train,
+    image: trainImg,
+  },
+  {
+    id: 3,
+    sound: cat,
+    image: catImg,
+  },
+  {
+    id: 4,
+    sound: book,
+    image: bookImg,
+  },
+  {
+    id: 5,
+    sound: lofi,
+    image: lofiImg,
+  },
+];
+
+export default cozy;
