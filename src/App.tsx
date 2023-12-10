a85f533bdacaca36271b7b6b55240138303099d6
Greg Pappas <gnpappas@proton.me>
2023-12-09 18:39:43 -1000

    Add backyard sounds

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Header from "./Header";
 import Home from "./Home";
+import Backyard from "./Backyard";
 import News from "./News";
 import Footer from "./Footer";
 
@@ -9,12 +10,12 @@ function App() {
     <div className="h-[100dvh] bg-purple-950/30 text-slate-200">
       <div className="mx-auto w-5/6">
         <Header />
-        <div className="bg-slate-950/50 grid grid-cols-1 md:grid-cols-3 gap-5 justify-center">
+        <div className="bg-slate-950/50 grid grid-cols-1 md:grid-cols-3 gap-5 justify-center p-5">
           <Routes>
             <Route path="/" element={<Home />} />
+            <Route path="/backyard" element={<Backyard />} />
           </Routes>
         </div>
-        <News/>
         <Footer />
       </div>
     </div>
