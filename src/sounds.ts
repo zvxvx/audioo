0335d333ea152a7f57ee4bfac3c0e3c07da78fc8
Greg Pappas <gnpappas@proton.me>
2023-12-09 16:13:27 -1000

    Make sounds obj to map out to other components

---
+++ src/sounds.ts
@@ -0,0 +1,24 @@
+import thunder from "./assets/audio/one-thunder.mp3";
+import lightning from "./assets/img/lightning.png?icon";
+import rain from "./assets/audio/two-rain.mp3";
+import rainImg from "./assets/img/rain.png?icon";
+import wind from "./assets/audio/three-wind.mp3";
+import windImg from "./assets/img/wind.png?icon";
+
+export const home = [
+  {
+    id: 0,
+    sound: thunder,
+    image: lightning,
+  },
+  {
+    id: 1,
+    sound: rain,
+    image: rainImg,
+  },
+  {
+    id: 2,
+    sound: wind,
+    image: windImg,
+  },
+];
