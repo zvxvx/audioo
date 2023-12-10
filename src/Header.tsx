231a3486856ae481b3d59ea885f9e47bc763b4f4
Greg Pappas <gnpappas@proton.me>
2023-12-09 17:48:01 -1000

    Remove news from menu

--- src/Header.tsx
+++ src/Header.tsx
@@ -17,8 +17,6 @@ const Header = () => {
         <ul className="flex gap-5 justify-center text-lg">
           <li><NavLink to={'/'} className={activeLink}
           >Home</NavLink></li>
-          <li><NavLink to={'/news'} className={activeLink}
-          >News</NavLink></li>
           <li><NavLink to={'/backyard'} className={activeLink}
           >Backyard</NavLink></li>
           <li><NavLink to={'/cozy'} className={activeLink}
