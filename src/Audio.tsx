37b5abdea0383a65ee4a58c83590c81f178b8f1e
Greg Pappas <gnpappas@proton.me>
2023-12-10 19:31:59 -1000

    Add volume slider tailwind

--- src/Audio.tsx
+++ src/Audio.tsx
@@ -18,7 +18,7 @@ const Audio = ({ url, img }: AudioProps) => {
     setVolume(Number(e.target.value));
   }
   return (
-    <div className="mx-auto flex flex-col">
+    <div className="mx-auto flex flex-col gap-5">
       <ReactPlayer
         url={url}
         loop={true}
@@ -31,9 +31,39 @@ const Audio = ({ url, img }: AudioProps) => {
         <img src={img} alt="icon" style={{ opacity: `${volume}` }} />
       </button>
       <input
+        className="w-full 
+        bg-transparent 
+        cursor-pointer 
+        appearance-none 
+        focus:outline-none
+        [&::-webkit-slider-thumb]:w-6
+        [&::-webkit-slider-thumb]:h-6
+        [&::-webkit-slider-thumb]:-mt-1
+        [&::-webkit-slider-thumb]:appearance-none
+        [&::-webkit-slider-thumb]:bg-slate-300
+        [&::-webkit-slider-thumb]:rounded
+        [&::-webkit-slider-thumb]:shadow-xl
+        
+        [&::-webkit-slider-runnable-track]:w-full
+        [&::-webkit-slider-runnable-track]:h-4
+        [&::-webkit-slider-runnable-track]:bg-slate-500
+        [&::-webkit-slider-runnable-track]:rounded
+
+
+        [&::-moz-range-thumb]:w-6
+        [&::-moz-range-thumb]:h-6
+        [&::-moz-range-thumb]:appearance-none
+        [&::-moz-range-thumb]:bg-slate-300
+        [&::-moz-range-thumb]:rounded
+        [&::-moz-range-thumb]:shadow-xl
+        
+        [&::-moz-range-track]:w-full
+        [&::-moz-range-track]:h-4
+        [&::-moz-range-track]:bg-slate-500
+        [&::-moz-range-track]:rounded"
+        id="basic-range-slider-usage"
         type="range"
         name="volume"
-        id="volume"
         min={0}
         max={1}
         step={0.01}
