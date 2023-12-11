afe17006642ec9dafdedfdc8830ae9bd30b47528
Greg Pappas <gnpappas@proton.me>
2023-12-11 08:53:05 -1000

    Consolidate sound objects into backyard

--- src/Backyard.tsx
+++ src/Backyard.tsx
@@ -1,17 +1,9 @@
 import Audio from "./Audio";
-import home from "./assets/playlists/home";
 import backyard from "./assets/playlists/backyard/backyard";
 
 const Backyard = () => {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5 p-5">
-      {home.map(({ id, sound, image }) => {
-        return (
-          <div key={id}>
-            <Audio url={sound} img={image} id={id} />
-          </div>
-        )
-      })}
       {backyard.map(({ id, sound, image }) => {
         return (
           <div key={id}>
