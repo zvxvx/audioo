11ebfc764991f77f11f5dbc17b727c85c678725c
Greg Pappas <gnpappas@proton.me>
2023-12-10 19:33:40 -1000

    Add news and to do list

--- src/News.tsx
+++ src/News.tsx
@@ -1,13 +1,21 @@
 const News = () => {
   return (
-    <>
+    <div className="pb-5 px-5">
+      <hr className="border border-slate-300/50 mb-2"/>
       <p className="text-2xl uppercase tracking-wide font-bold">News</p>
-      <article className="border-t border-purple-300/50">
-        <p className="italic text-lg">{new Date(2023, 11, 9).toDateString()}</p>
-        <p>Well it's been a long time coming and it's finally here. Don't know how many visitors there's been in the past, but the site should be a lot faster and more functional than ever before. It's also been a long time since any new playlists have been added, but since I've rebuilt the site, I can do a lot more with a lot less work, so be on the look out for that.</p>
-        <p>Also, if you're interested in contributing sounds, please feel free to do so through the google form. However, there's no guarantee they'll be added. -g</p>
+      <article>
+        <p className="italic text-lg">{new Date(2023, 11, 10).toDateString()}</p>
+        <p className="mt-1">Well it's been a long time coming and it's finally here. Don't know how many visitors there's been in the past, but the site should be a lot faster and more functional than ever before. It's also been a long time since any new playlists have been added, but since I've rebuilt the site, I can do a lot more with a lot less work, so be on the look out for that.</p>
+        <p className="mt-3">Also, if you're interested in contributing sounds, I'll be creating a form for you to do so in the near future.</p>
+        <hr className="border border-slate-300/50 my-2"/>
+        <p className="text-2xl uppercase tracking-wide font-bold">Things to add</p>
+        <ul className="list-disc pl-5">
+          <li>Playlist color schemes</li>
+          <li>More playlists</li>
+          <li>Sound submission form</li>
+        </ul>
       </article>
-    </>
+    </div>
   )
 }
 
