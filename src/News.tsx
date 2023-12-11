811dec7e6d728acd7682f059bbb0e6c389ccc9f7
Greg Pappas <gnpappas@proton.me>
2023-12-11 13:43:42 -1000

    Remove to do list

--- src/News.tsx
+++ src/News.tsx
@@ -7,13 +7,6 @@ const News = () => {
         <p className="italic text-lg">{new Date(2023, 11, 10).toDateString()}</p>
         <p className="mt-1">Well it's been a long time coming and it's finally here. Don't know how many visitors there's been in the past, but the site should be a lot faster and more functional than ever before. It's also been a long time since any new playlists have been added, but since I've rebuilt the site, I can do a lot more with a lot less work, so be on the look out for that.</p>
         <p className="mt-3">Also, if you're interested in contributing sounds, I'll be creating a form for you to do so in the near future.</p>
-        <hr className="border border-slate-300/50 my-2"/>
-        <p className="text-2xl uppercase tracking-wide font-bold">Things to add</p>
-        <ul className="list-disc pl-5">
-          <li>Playlist color schemes</li>
-          <li>More playlists</li>
-          <li>Sound submission form</li>
-        </ul>
       </article>
     </div>
   )
