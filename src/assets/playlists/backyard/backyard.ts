d80743a6f4e8c57cd8471fca9f25df42d70f9222
Greg Pappas <gnpappas@proton.me>
2023-12-11 08:52:43 -1000

    Consolidate home into backyard since home shares backyard assets

--- src/assets/playlists/backyard/backyard.ts
+++ src/assets/playlists/backyard/backyard.ts
@@ -1,3 +1,9 @@
+import thunder from "./audio/one-thunder.mp3";
+import lightning from "./img/lightning.png?icon";
+import rain from "./audio/two-rain.mp3";
+import rainImg from "./img/rain.png?icon";
+import wind from "./audio/three-wind.mp3";
+import windImg from "./img/wind.png?icon";
 import insects from "./audio/five-insects.mp3";
 import insImg from "./img/insect.png?icon";
 import chimes from "./audio/four-chimes.mp3";
@@ -6,6 +12,21 @@ import water from "./audio/six-water.mp3";
 import watImg from "./img/water.png?icon";
 
 const backyard = [
+  {
+    id: 0,
+    sound: thunder,
+    image: lightning,
+  },
+  {
+    id: 1,
+    sound: rain,
+    image: rainImg,
+  },
+  {
+    id: 2,
+    sound: wind,
+    image: windImg,
+  },
   {
     id: 3,
     sound: chimes,
