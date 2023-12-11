660ca6a2bc1d1e316a7fced1532b7e407a50d55b
Greg Pappas <gnpappas@proton.me>
2023-12-11 08:53:18 -1000

    Slice backyard for home sound examples

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,12 +1,12 @@
 import Audio from "./Audio";
 import News from "./News";
-import home from "./assets/playlists/home";
+import backyard from "./assets/playlists/backyard/backyard";
 
 const Home = () => {
   return (
     <>
       <div className="grid grid-cols-1 sm:grid-cols-3 gap-5 p-5 justify-center">
-        {home.map(({ id, sound, image }) => {
+        {backyard.slice(0, 3).map(({ id, sound, image }) => {
           return (
             <div key={id}>
               <Audio url={sound} img={image} id={id} />
