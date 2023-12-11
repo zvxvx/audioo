57d45e72b1fd6d6c6501b9906210a21e3e8a3d45
Greg Pappas <gnpappas@proton.me>
2023-12-10 19:32:58 -1000

    Adjust footer styling

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
-    <div className="bg-slate-950/60 text-center rounded-b">
-      <a href="https://pappas.sh" target="_blank" className="uppercase">~/p/<span>{new Date().getFullYear().toString()} </span></a>
+    <div className="bg-slate-950/60 text-center rounded-b py-2">
+      <a href="https://pappas.sh" target="_blank" className="uppercase font-bold tracking-widest text-sm">~/p/<span>{new Date().getFullYear().toString()} </span></a>
     </div>
   )
 }
