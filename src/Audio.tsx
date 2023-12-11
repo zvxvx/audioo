66136673d35afee09032374313746171231d3709
Greg Pappas <gnpappas@proton.me>
2023-12-11 06:17:17 -1000

    Back to original controls

--- src/Audio.tsx
+++ src/Audio.tsx
@@ -18,14 +18,8 @@ const Audio = ({ url, img }: AudioProps) => {
     setVolume(Number(e.target.value));
   }
 
-  function touchVolume(e: TouchEvent<HTMLInputElement>) {
-    if (e.currentTarget instanceof HTMLInputElement) {
-      setVolume(Number(e.currentTarget.value));
-    }
-  }
-
   return (
-    <div className="mx-auto flex flex-col">
+    <div className="mx-auto flex flex-col gap-5">
       <ReactPlayer
         url={url}
         loop={true}
@@ -38,16 +32,44 @@ const Audio = ({ url, img }: AudioProps) => {
         <img src={img} alt="icon" style={{ opacity: `${volume}` }} />
       </button>
       <input
+        className="w-full 
+        bg-transparent 
+        cursor-pointer 
+        appearance-none 
+        focus:outline-none
+        [&::-webkit-slider-thumb]:w-6
+        [&::-webkit-slider-thumb]:h-6
+        [&::-webkit-slider-thumb]:-mt-1
+        [&::-webkit-slider-thumb]:appearance-none
+        [&::-webkit-slider-thumb]:bg-slate-300
+        [&::-webkit-slider-thumb]:rounded
+        [&::-webkit-slider-thumb]:shadow-xl
+        
+        [&::-webkit-slider-runnable-track]:w-full
+        [&::-webkit-slider-runnable-track]:h-4
+        [&::-webkit-slider-runnable-track]:bg-slate-500
+        [&::-webkit-slider-runnable-track]:rounded
+
+
+        [&::-moz-range-thumb]:w-6
+        [&::-moz-range-thumb]:h-6
+        [&::-moz-range-thumb]:appearance-none
+        [&::-moz-range-thumb]:bg-slate-300
+        [&::-moz-range-thumb]:rounded
+        [&::-moz-range-thumb]:shadow-xl
+        
+        [&::-moz-range-track]:w-full
+        [&::-moz-range-track]:h-4
+        [&::-moz-range-track]:bg-slate-500
+        [&::-moz-range-track]:rounded"
+        id="basic-range-slider-usage"
         type="range"
         name="volume"
-        id="volume"
         min={0}
         max={1}
         step={0.01}
         value={volume}
         onChange={changeVolume}
-        onTouchStart={touchVolume}
-        onTouchMove={touchVolume}
       />
     </div>
   );
