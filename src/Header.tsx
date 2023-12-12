9033877fdb4ff390c4bfa25eed943ecffa69d120
Greg Pappas <gnpappas@proton.me>
2023-12-11 18:48:14 -1000

    Add a more distinct active link

--- src/Header.tsx
+++ src/Header.tsx
@@ -8,12 +8,12 @@ const Header = () => {
   }
 
   const activeLink = ({ isActive, isPending }: ActiveProps) =>
-    isPending ? "" : isActive ? "font-bold border-b-4 border-white/30" : "";
+    isPending ? "" : isActive ? "font-bold border-b-4 border-white/30 bg-white/10 py-2.5" : "";
 
   return (
     <div className="flex flex-col items-center">
       <img src={logo} alt="audioo" className="w-1/2 my-5" />
-      <nav className="w-full bg-slate-950/60 rounded-t py-2">
+      <nav className="w-full bg-slate-950/60 rounded-t py-2 shadow-xl">
         <ul className="flex gap-3 justify-center sm:text-xl">
           <li><NavLink to={'/'} className={activeLink}
           >Home</NavLink></li>
