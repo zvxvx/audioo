976f89bad092aadc6ec60a3a31ea20e0ae87dddc
Greg Pappas <gnpappas@proton.me>
2023-12-12 08:05:45 -1000

    Remove height param

--- src/Backyard.tsx
+++ src/Backyard.tsx
@@ -4,7 +4,7 @@ const Backyard = () => {
   document.documentElement.style.setProperty('background', '#40372f')
   document.documentElement.style.setProperty('color', '#6fc298')
   return (
-    <div className="min-h-[512px] grid grid-cols-1 md:grid-cols-3 sm:grid-cols-2 gap-5 pb-5 px-5 justify-center">
+    <div className="grid grid-cols-1 md:grid-cols-3 sm:grid-cols-2 gap-5 pb-5 px-5 justify-center">
       {backyard.map(({ id, sound, image }) => {
         return (
           <div key={id}>
