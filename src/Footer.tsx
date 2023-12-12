103a9a5b41b110448d2e4ae4520e8452e0d1ea10
Greg Pappas <gnpappas@proton.me>
2023-12-11 21:04:10 -1000

    Add subtle shadow

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <div className="bg-slate-950/60 text-center rounded-b py-2">
+    <div className="bg-slate-950/60 text-center rounded-b py-2 shadow-xl">
       <a href="https://pappas.sh" target="_blank" className="uppercase font-bold tracking-widest text-sm">~/p/<span>{new Date().getFullYear().toString()} </span></a>
     </div>
   )
