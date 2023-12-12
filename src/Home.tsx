05b365402872d1481da64e3035acf8c86a92359c
Greg Pappas <gnpappas@proton.me>
2023-12-11 17:55:34 -1000

    Adjust text and bg for contrast

--- src/Home.tsx
+++ src/Home.tsx
@@ -3,8 +3,8 @@ import News from "./News";
 import backyard from "./assets/playlists/backyard/backyard";
 
 const Home = () => {
-  document.documentElement.style.setProperty('background', '#58656D');
-  document.documentElement.style.setProperty('color', '#ffafa0');
+  document.documentElement.style.setProperty('background', '#40535e');
+  document.documentElement.style.setProperty('color', '#f2b5aa');
   return (
     <>
       <div className="grid grid-cols-1 sm:grid-cols-3 gap-5 pb-5 px-5 justify-center">
