dac962c8898c23746db1c2b7a67115025c0343e0
Greg Pappas <gnpappas@proton.me>
2023-12-11 21:04:18 -1000

    Make brown muted

--- src/Backyard.tsx
+++ src/Backyard.tsx
@@ -1,10 +1,10 @@
 import Audio from "./Audio";
 import backyard from "./assets/playlists/backyard/backyard";
 const Backyard = () => {
-  document.documentElement.style.setProperty('background', '#5b422b')
+  document.documentElement.style.setProperty('background', '#40372f')
   document.documentElement.style.setProperty('color', '#6fc298')
   return (
-    <div className="grid grid-cols-1 md:grid-cols-3 sm:grid-cols-2 gap-5 pb-5 px-5 justify-center">
+    <div className="min-h-[512px] grid grid-cols-1 md:grid-cols-3 sm:grid-cols-2 gap-5 pb-5 px-5 justify-center">
       {backyard.map(({ id, sound, image }) => {
         return (
           <div key={id}>
