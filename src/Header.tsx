26bab34baa1b6cfffec23a5c12268087608b2728
Greg Pappas <gnpappas@proton.me>
2023-12-11 18:55:38 -1000

    Add x padding for active/pending

--- src/Header.tsx
+++ src/Header.tsx
@@ -8,13 +8,13 @@ const Header = () => {
   }
 
   const activeLink = ({ isActive, isPending }: ActiveProps) =>
-    isPending ? "" : isActive ? "font-bold border-b-4 border-white/30 bg-white/10 py-2.5" : "";
+    isPending ? "" : isActive ? "font-bold border-b-4 border-white/30 bg-white/10 py-2.5 px-1" : "px-1";
 
   return (
     <div className="flex flex-col items-center">
       <img src={logo} alt="audioo" className="w-1/2 my-5" />
       <nav className="w-full bg-slate-950/60 rounded-t py-2 shadow-xl">
-        <ul className="flex gap-3 justify-center sm:text-xl">
+        <ul className="flex gap-2 justify-center sm:text-xl">
           <li><NavLink to={'/'} className={activeLink}
           >Home</NavLink></li>
           <li><NavLink to={'/backyard'} className={activeLink}
