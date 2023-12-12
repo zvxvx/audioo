4deff36054ef76eafad2604e61f5bae748449a9a
Greg Pappas <gnpappas@proton.me>
2023-12-12 12:35:31 -1000

    Organize playlist components into own folder

---
+++ src/playlists/Sleep.tsx
@@ -0,0 +1,20 @@
+import Audio from '../Audio';
+import sleep from '../assets/playlists/sleep/sleep';
+
+const Sleep = () => {
+  document.documentElement.style.setProperty('background', '#19202f');
+  document.documentElement.style.setProperty('color', '#4cc594');
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-3 sm:grid-cols-2 gap-5 pb-5 px-5 justify-center">
+      {sleep.map(({ id, sound, image }) => {
+        return (
+          <div key={id}>
+            <Audio url={sound} img={image} id={id} />
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+export default Sleep
